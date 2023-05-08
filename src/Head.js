72febea5c1f3525fe91ffa46fab2ad085545109f
Ratan Kumar <rk2999643@gmail.com>
2023-05-08 09:16:58 +0530

    commit

---
+++ src/Head.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.css";
+
+function Head() {
+  return (
+    <div className='textform'>
+        <h1 style={{fontSize:'60px',color:'blue',fontFamily:'-moz-initial'}}>EMPLOYEE FEEDBACK FORM</h1>
+        </div>
+  )
+}
+
+export default Head
